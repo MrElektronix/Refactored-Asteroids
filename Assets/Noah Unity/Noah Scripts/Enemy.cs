aa715380a2691a996818c338e4f7f38039228a14
epicacid <noahsenteur12345@gmail.com>
2016-04-22 13:55:28 +0200

    SpawningScript

---
+++ Assets/Noah Unity/Noah Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class Enemy : MonoBehaviour {
+	
+
+
+
+
+
+	public int Health = 100;
+	public int HitPoints = 25;
+	public int Speed = 10;
+
+
+
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {	
+		transform.Translate (Vector3.forward * Time.deltaTime);
+
+	
+	}
+}
---
+++ Assets/Noah Unity/Noah Scripts/LoadOnScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadOnScript : MonoBehaviour {
+	public string GameScene;
+
+
+	public void LoadOnClick()
+	{
+		
+		SceneManager.LoadScene (GameScene);
+		
+	}
+
+}
