71f9c0cb5223200c6f113f4108c81c799cf0c54d
MrElektronix <milovanpelt@gmail.com>
2016-04-22 12:10:52 +0100

    sssss

---
+++ Assets/Milo Unity/Milo Scripts/Movement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	public int z = 0;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(0f, 0f, z);
+		Debug.Log (z);
+		z--;
+	}
+}
