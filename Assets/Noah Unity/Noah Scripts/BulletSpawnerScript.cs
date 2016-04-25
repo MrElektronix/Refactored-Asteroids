f684fde8abae30d1d8bcfae27fa1a33d1acc3b7d
epicacid <noahsenteur12345@gmail.com>
2016-04-25 18:14:56 +0200

    Schieten
    De Player kan schieten met de Space Bar input dit moet uiteindelijk
    veranderd worden naar een scherm input. De bullet wordt afgeschoten maar
    te zacht dit kan veranderd worden met de public speed int die zijn
    aangemaakt.

---
+++ Assets/Noah Unity/Noah Scripts/BulletSpawnerScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletSpawnerScript : MonoBehaviour {
+
+	public GameObject bullet;
+	public GameObject muzzle;
+	public float bulletforce;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			GameObject TempBullet;
+			TempBullet = Instantiate (bullet, muzzle.transform.position, muzzle.transform.rotation) as GameObject;
+			TempBullet.transform.Rotate (Vector3.forward * 90);
+			Rigidbody TempRigidbody;
+			TempRigidbody = TempBullet.GetComponent<Rigidbody> ();
+			TempRigidbody.AddForce (transform.forward * bulletforce);
+			Destroy (TempBullet, 10.0f);
+
+
+				
+
+
+		}
+
+	
+	}
+}
