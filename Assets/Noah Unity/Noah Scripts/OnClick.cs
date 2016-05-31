ab7cd5144dd0f4e48ecc6891bfddabc4a817bfed
epicacid <noahsenteur12345@gmail.com>
2016-05-31 12:20:09 +0200

    Main menu + Buttons

---
+++ Assets/Noah Unity/Noah Scripts/OnClick.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class OnClick : MonoBehaviour {
+
+	public void LoadLevel()
+	{
+		SceneManager.LoadScene ("0");
+
+	}
+
+
+	// Use this for initialization
+
+}
