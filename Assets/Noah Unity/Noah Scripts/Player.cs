6e5267a49ef3206e0800a16d61b1a08178276955
epicacid <noahsenteur12345@gmail.com>
2016-06-17 10:01:29 +0200

    Healthbar

--- Assets/Noah Unity/Noah Scripts/Player.cs
+++ Assets/Noah Unity/Noah Scripts/Player.cs
@@ -5,14 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour {
 
-	public static int P_hp = 100;
+	public float P_hp = 100f;
+	public float curHealth = 0f;
 	public int Death = 0;
+	public GameObject bar;
+
+
+
 
 
 
 	// Use this for initialization
 	void Start () {
-		
+		curHealth = P_hp;
+
+	
 	
 	}
 	void OnTriggerEnemy(Collider other)
@@ -22,16 +29,32 @@ public class Player : MonoBehaviour {
 			Destroy (other.gameObject); //Destroy de enemy als die in aanraking komt met de player
 			P_hp -= 25; //Haal 25 hp van de player af
 			print (P_hp); //Print HP 
+			decreasehealth();
+
 			}
 		if(other.gameObject.tag == "Bullet")
 		{
 			P_hp -= 25;
 			print (P_hp);
+			decreasehealth ();
 
 
 		}
 	
 	}
+	public void decreasehealth()
+	{
+		curHealth -= 25f;
+		float calcHealth = curHealth / P_hp;
+		setHealth (calcHealth);
+
+	}
+	public void setHealth(float myHealth)
+	{
+		bar.transform.localScale = new Vector3 (myHealth,bar.transform.localScale.y,bar.transform.localScale.z);
+	}
+
+
 	void Update()
 	{
 		//if (P_hp <= Death)
