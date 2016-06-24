cd0e6a113afd6a7d967fd23cea55b5cb54e4dcc4
MrElektronix <milovanpelt@gmail.com>
2016-06-24 14:14:04 +0200

    Git works

---
+++ Assets/Milo Unity/Milo Scripts/BackToMainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BackToMainMenu : MonoBehaviour {
+
+	//GameObject MainMenu;
+	//GameObject PauseScreen;
+
+
+	public void Start() {
+		//MainMenu = GameObject.Find("Main Menu Omhulsel");
+		//PauseScreen = GameObject.Find("Pause Omhulsel");
+	}
+
+	public void LoadMain() {
+		SceneManager.LoadScene (0);
+		//MainMenu.SetActive(true);
+		//PauseScreen.SetActive(false);
+		//Time.timeScale = 0;
+	}
+}
